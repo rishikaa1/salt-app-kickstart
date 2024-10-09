74cd20bf1415f13905251660d82e2870f1c08fdf
Rishika Hazarika <131283498+rishikaa1@users.noreply.github.com>
2024-10-09 23:42:47 +0530

    Update push_to_github.js

--- push_to_github.js
+++ push_to_github.js
@@ -4,8 +4,10 @@ import util from 'util'
 import simpleGit from 'simple-git'
 import path from 'path'
 import exec_childprocess from './exec_childprocess.js'
+import inquirer from 'inquirer'
 
 const execPromise = util.promisify(exec);
+
   async function checkAndInitializeGitRepo(directory) {
     try {
       // Check if the directory is a Git repository
@@ -26,29 +28,89 @@ const execPromise = util.promisify(exec);
       }
     }
   }
-const push_to_github=async (projectName,token,username,repositoryName,branch,commitMessage)=>{
-    console.log(chalk.green("Pushing to github.."));
-    const remote=`https://${token}@github.com/${username}/${repositoryName}.git`;
-    const commands=[
-        `cd ${projectName}`,
-        `gh repo create ${repositoryName} --public --description ${repositoryName}`,
-        'git init',
-        'git add .',
-        `git commit -m ${commitMessage}`,
-        // `git remote get-url origin`,
-        `git remote add origin ${remote}`,
-        `git push origin ${branch}`
-    ];
-    let directory=path.dirname(process.cwd())+path.sep+projectName;
-    const fullCommand = commands.join(' && ');
-    try{
-        await checkAndInitializeGitRepo()
-        await exec_childprocess(fullCommand,{ shell: true });
-        console.log(chalk.green('Pushed to remote repository'));    
-    }catch(err){
-    console.log("Error: "+err.message);
-}
+
+const getGitHubCredentials = async () => {
+  consr questions = [
+    {
+      type: 'input',
+      name: 'username',
+      message: 'Enter your GitHub username:',
+      validate: input => input.trim() !== '' || 'Username cannot be empty'
+    },
+    {
+      type: 'password',
+      name: 'token',
+      message: 'Enter your GitHub Personal Access Token:',
+      mask: '*',
+      validate: input => input.trim() !== '' || 'Token cannot be empty'
+    },
+    {
+      type: 'input',
+      name: 'repositoryName',
+      message: 'Enter the repository name:',
+      validate: input => input.trim() !== '' || 'Repository name cannot be empty'
+    },
+    {
+      type: 'list',
+      name: 'branch',
+      message: 'Select the branch name:',
+      choices: ['master', 'main', 'develop', new inquirer.Separator(), 'Custom'],
+      default: 'main'
+    },
+    {
+      type: 'input',
+      name: 'customBranch',
+      message: 'Enter the custom branch name:',
+      when: answers => answers.branch === 'Custom',
+      validate: input => input.trim() !== '' || 'Branch name cannot be empty'
+    },
+     {
+      type: 'input',
+      name: 'commitMessage',
+      message: 'Enter the commit message:',
+      default: 'Initial commit'
+    }
+  ];
+
+  return inquirer.prompt(questions);
 };
+
+const push_to_github=async (projectName, token, username, repositoryName, branch, commitMessage, isAutoPush = false) => {
+  console.log(chalk.green("Pushing to github.."));
+
+  let gitConfig;
+  if (isAutoPush) {
+    gitConfig = { token, username, repositoryName, branch, commitMessage };
+  } else {
+    gitConfig = await getGitHubCredentials();
+    if (gitConfig.branch == 'Custom') {
+      gitConfig.branch = gitConfig.customBranch;
+    }
+  }
+  
+  const remote=`https://${gitConfig.token}@github.com/${gitConfig.username}/${gitConfig.repositoryName}.git`;
+  const commands=[
+    `cd ${projectName}`,
+    `gh repo create ${gitConfig.repositoryName} --public --description ${gitConfig.repositoryName}`,
+    'git init',
+    'git add .',
+    `git commit -m "${gitConfig.commitMessage}"`,
+    `git remote add origin ${remote}`,
+    `git push origin ${branch}`
+  ];
+  
+  let directory = path.join(process.cwd(), projectName);
+  const fullCommand = commands.join(' && ');
+  
+  try {
+    await checkAndInitializeGitRepo(directory);
+    await exec_childprocess(fullCommand,{ shell: true });
+    console.log(chalk.green('Pushed to remote repository successfully.'));    
+  } catch(err) {
+    console.log(chalk.red("Error: "+err.message));
+  }
+};
+
 export default push_to_github;
 
 
