32b4cac5b3e32f91855dce624fdec4ce3f79e251
Rishika Hazarika <131283498+rishikaa1@users.noreply.github.com>
2024-10-09 23:49:44 +0530

    Update install_dependencies.js

--- install_dependencies.js
+++ install_dependencies.js
@@ -2,7 +2,11 @@ import {exec} from 'child_process'
 import chalk from 'chalk'
 import util from 'util'
 import exec_childprocess from './exec_childprocess.js';
+import path from 'path';
+import fs from 'fs-extra';
+
 const execPromise = util.promisify(exec);
+
 const dependencies=[
     {
       name:'react',
@@ -46,26 +50,40 @@ const dependencies=[
     }
 ];
 
-const install_dependencies=async (projectName)=>{
+const install_dependencies = async (projectName, customDirectory = './') => {
     console.log(chalk.green("Installing Dependencies..."));
-    const install_command=`npm install ${dependencies.map(dep=>{
-        const {name,version}=dep;
+    
+    const install_command=`npm install ${dependencies.map(dep => {
+        const {name, version} = dep;
         console.log(`${name}@${version}\n`);
         return `${name}@${version}`
-        }).join(' ')}`;
+    }).join(' ')}`;
+
+    const fullPath = path.join(customDirectory, projectName);
+
+    await fs.ensureDir(fullPath);
+    
     const commands=[
-        `mkdir ${projectName} && cd ${projectName}`,
+        `cd "${fullPath}"`,
         'npx create-react-app ./',
         install_command
         // `npm install react`
     ];
+    
     const fullCommand = commands.join(' && ');
     console.log(fullCommand);
-    try{
-        await exec_childprocess(fullCommand,{ shell: true });
-        console.log(chalk.green('Dependencies installed Successfully.'));
-    }catch(err){
-    console.log("Error: "+err.message);
-}
+    
+    try {
+        await exec_childprocess(fullCommand, { shell: true });
+        console.log(chalk.green('Dependencies installed successfully.'));
+
+        const packageJsonPath = path.join(fullPath, 'package.json');
+        const packageJson = await fs.readJson(packageJsonPath);
+        packageJson.name = projectName;
+        await fs.writeJson(packageJsonPath, packageJson, { spaces: 2 });
+        console.log(chalk.green('Updated package.json with custom project name.'));
+    } catch(err) {
+        console.log(chalk.red("Error: "+err.message));
+    }
 };
 export default install_dependencies;
