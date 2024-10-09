ca0131ab120c33d69289fae15020d66fc20bfc73
Rishika Hazarika <131283498+rishikaa1@users.noreply.github.com>
2024-10-09 23:29:34 +0530

    Update copy_templates.js

--- copy_templates.js
+++ copy_templates.js
@@ -1,56 +1,61 @@
 import fs from 'fs-extra';
 import path from 'path';
 import { fileURLToPath } from 'url';
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
 async function copyFolder(projectName,template_choices) {
   try {
-    let relativePath=`${path.sep+"templates"+path.sep+template_choices.toLowerCase()}`
-    let sourcePath=__dirname;
-    let source=sourcePath+relativePath;
-    let basePath=process.cwd();
-    let destinationPath=`${projectName+path.sep+"src"}`;
-    let destination=basePath+path.sep+destinationPath+relativePath;
-    await fs.copy(source, destination);
-    let files_array=["index.js","index.html"];
-    let app_file_content={
-        form:
-            'import Form from "./templates/form/Form.js";\n'+
-            'function App() {\n'+
-            'return <Form/>\n;'+
-            '}\n'+
-            'export default App;\n'
-        ,
-        aggrid:
-            'import { Default } from "./templates/aggrid/AgGrid.js";\n'+
-            'function App() {\n'+
-            'return <Default/>;\n'+
-            '}\n'+
-            'export default App;\n'
-        ,
-        appheader:
-            'import AppHeader from "./templates/appheader/AppHeader.js";\n'+
-            'function App() {\n'+
-            'return <AppHeader/>;\n'+
-            '}\n'+
-            'export default App;\n'
+    const basePath = process.cwd();
+    const destinationPath = `${projectName}${path.sep}src`;
+
+    const templates = Array.isArray(template_choices) ? template_choices : [template_choices];
+
+    for (const template of templates) {
+      const relativePath = `${path.sep}templates${path.sep}${template.toLowerCase()}`;
+      const sourcePath = __dirname;
+      const source = sourcePatj + relativePath;
+      const destination = path.join(basePath, destinationPath, relativePath);
+
+      await fs.copy(source, destination);
     }
-    let appJsPath=basePath+path.sep+destinationPath+path.sep+"App.js";
-    let content=app_file_content[template_choices.toLowerCase()]; 
-    fs.writeFile(appJsPath, content, 'utf-8', (err) => {
-        if (err) {
-          console.error(`Error writing to App.js: ${err.message}`);
-          return;
-        }
-        console.log(`Content has been successfully written to ${appJsPath}`);
-      });
-    files_array.map(async(filePath)=>{
-        let fileSource=sourcePath+path.sep+filePath;
-        let fileDestination=basePath+path.sep+destinationPath+path.sep+filePath;
-        await fs.copy(fileSource, fileDestination,{overwrite:true});
-    });
+
+    const files_array = ["index.js", "index.html"];
+    for (const filePath of files_array) {
+      const fileSource = path.join(__dirname, filePath);
+      const fileDestination = path.join(basePath, destinationPath, filePath);
+      await fs.copy(fileSource, fileDestination, { overwrite: true });
+    }
+
+    let appJsContent = "import React from 'react';\n\n";
+    let allJsRemder = "return (\n <div>\n";
+
+    const app_file_content = {
+      form: "import Form from './templates/form/Form.js';\n",
+      aggrid: "import { Default as AgGrid } from './templates/aggrid/AgGrid.js';\n",
+      appheader: "import AppHeader from './templates/appheader/AppHeader.js';\n"
+    };
+
+    for (const template of templates) {
+      const lowerTemplate = template.toLowerCase();
+      if (app_file_content[lowerTemplate]) {
+        appJsContent += app_file_content[lowerTemplate];
+        appJsRender += `    <${template} />\n`;
+      }
+    }
+    
+    appJsContent += "\nfunction App() {\n";
+    appJsContent += appJsRender;
+    appJsContent += "  </div>\n);\n}\n\nexport default App;\n";
+
+    const appJsPath = path.join(basePath, destinationPath, "App.js");
+    await fs.writeFile(appJsPath, appJsContent, 'utf-8');
+    console.log(`Content has been successfully written to ${appJsPath}`);
+
   } catch (err) {
     console.error(`Error copying folder: ${err}`);
   }
 }
+
 export default copyFolder;
