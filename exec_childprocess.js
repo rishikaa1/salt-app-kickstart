6b0438a2b3fcf4a3408c12cc85549f4baedfe907
Rishika Hazarika <131283498+rishikaa1@users.noreply.github.com>
2024-10-10 15:21:34 +0530

    Update exec_childprocess.js

--- exec_childprocess.js
+++ exec_childprocess.js
@@ -1,24 +1,29 @@
-import {exec} from 'child_process'
-import chalk from 'chalk'
-import util from 'util'
-const exec_childprocess=(command,options={})=>{
+import { exec } from 'child_process';
+import chalk from 'chalk';
+import util from 'util';
+
+const exec_childprocess = (command, options = {}) => {
   return new Promise((resolve, reject) => {
     const childProcess = exec(command, options);
+
     childProcess.stdout.on('data', (data) => {
-      console.log(chalk.white(data.toString()));
+      console.log(chalk.white(data.toString())); // logs standard output
     });
+
     childProcess.stderr.on('data', (data) => {
-      console.error(chalk.red(data.toString()));
+      console.error(chalk.red(data.toString())); // logs standard error
     });
+
     childProcess.on('close', (code) => {
       if (code === 0) {
-        resolve();
+        resolve(); // successfully executed
       } else {
-        reject(new Error(`Process exited with code: ${code}`));
+        reject(new Error(`Process exited with code: ${code} for command: ${command}`)); // error context
       }
     });
+
     childProcess.on('error', (err) => {
-      reject(err);
+      reject(new Error(`Failed to execute command: ${command}. Error: ${err.message}`)); // error message
     });
   });
 }
