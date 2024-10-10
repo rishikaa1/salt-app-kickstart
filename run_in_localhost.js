c18ef5a3a59862a1f44ee445378e923a865b0371
Rishika Hazarika <131283498+rishikaa1@users.noreply.github.com>
2024-10-10 12:11:42 +0530

    Update run_in_localhost.js

--- run_in_localhost.js
+++ run_in_localhost.js
@@ -2,21 +2,28 @@ import {exec} from 'child_process'
 import chalk from 'chalk'
 import util from 'util'
 import exec_childprocess from './exec_childprocess.js';
+import path from 'path';
+
 const execPromise = util.promisify(exec);
 
-const run_in_localhost=async (projectName)=>{
+const run_in_localhost = async (projectName, customDirectory = './') => {
     console.log(chalk.green("Launching to Localhost.."));
+
+    const fullPath = path.join(customDirectory, projectName);
+    
     const commands=[
-        `cd ${projectName}`,
+        `cd "${fullPath}"`,
         'npm start',
     ];
+    
     const fullCommand = commands.join(' && ');
-    console.log(fullCommand);
+    console.log(chalk.blue('Executing command:'), fullCommand);
+    
     try{
         await exec_childprocess(fullCommand,{ shell: true });
         console.log(chalk.green('Running on Localhost'));    
-    }catch(err){
-    console.log("Error: "+err.message);
-}
+    } catch(err) {
+        console.log("Error: "+err.message);
+    }
 };
 export default run_in_localhost;
